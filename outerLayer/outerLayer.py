2f064d171ea0701d88e218bc21cdb381e7d39f4a
Henry <henrypdt.1310@gmail.com>
2024-04-15 09:26:43 +1000

    Fix some SQL code that causes lock when large traffic

--- outerLayer/outerLayer.py
+++ outerLayer/outerLayer.py
@@ -89,13 +89,13 @@ class OuterLayer():
                     log_name = f"{threat_name}-{event['timestamp']}"
                     self.add_threat(ip, log_name, event['geolocation'], event['timestamp'], threat_name)
                     count = 0
-                self.database.execute_query(f"UPDATE hybrid_idps.outerLayer SET processed = True WHERE ip_address = %s AND event_type = %s", (ip, event_type))
+                self.database.execute_query(f"UPDATE hybrid_idps.outerLayer SET processed = True WHERE id = %s", (event['id'],))
 
 
     def analyze_port_scanning(self):
         event_type = 'Possible Port Scanning'
         threat_name = "Port Scanning"
-        threshold = 200
+        threshold = 100
         self.analyze_event_type(event_type, threat_name, threshold)
 
 
@@ -122,9 +122,8 @@ class OuterLayer():
         threshold = 10  # Placeholder threshold, adjust as needed
         
         for event_type in event_types:
-            results = self.database.execute_query(f"SELECT * FROM hybrid_idps.outerLayer WHERE event_type = '{event_type}' AND processed = False ORDER BY timestamp DESC")
+            results = self.database.execute_query(f"SELECT * FROM hybrid_idps.outerLayer WHERE event_type = %s AND processed = False ORDER BY timestamp DESC", (event_type,))
             results = self.extract_ips(results)
-           
 
             for ip, all_events in results.items():
                 count = 0
@@ -137,8 +136,7 @@ class OuterLayer():
                             logName = f"{threatName}-{event['timestamp']}"
                             self.add_threat(ip, logName, event['geolocation'], event['timestamp'], threatName)
                             count = 0
-                        
-                        self.database.execute_query(f"UPDATE hybrid_idps.outerLayer SET processed = True WHERE ip_address = '{ip}' AND event_type = '{event_type}'")
+                        self.database.execute_query(f"UPDATE hybrid_idps.outerLayer SET processed = True WHERE id = %s", (event['id'],))
 
     def analyze_unusual_outgoing_geolocation(self): 
         event_types = ['Suspicious Port Activity']
@@ -148,8 +146,9 @@ class OuterLayer():
         threshold = 10  # Placeholder threshold, adjust as needed
         
         for event_type in event_types:
-            results = self.database.execute_query(f"SELECT * FROM hybrid_idps.outerLayer WHERE event_type = '{event_type}' AND processed = False ORDER BY timestamp DESC")
+            results = self.database.execute_query(f"SELECT * FROM hybrid_idps.outerLayer WHERE event_type = %s AND processed = False ORDER BY timestamp DESC", (event_type,))
             results = self.extract_ips(results)
+           
             for ip, all_events in results.items():
                 count = 0
                 for event in all_events:
@@ -161,8 +160,7 @@ class OuterLayer():
                             logName = f"{threatName}-{event['timestamp']}"
                             self.add_threat(ip, logName, event['geolocation'], event['timestamp'], threatName)
                             count = 0
-                        
-                        self.database.execute_query(f"UPDATE hybrid_idps.outerLayer SET processed = True WHERE ip_address = '{ip}' AND event_type = '{event_type}'")
+                        self.database.execute_query(f"UPDATE hybrid_idps.outerLayer SET processed = True WHERE id = %s", (event['id'],))
     
 
     # def display_Events_and_calc_threat_level(self):
