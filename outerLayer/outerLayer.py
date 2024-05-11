6a73dc59f90aee369f848c33c99e9e7fa34b964a
Joshua-E-ONeill <edenoneill2@gmail.com>
2024-05-12 03:40:03 +1000

    Bot Army Detection

--- outerLayer/outerLayer.py
+++ outerLayer/outerLayer.py
@@ -1,5 +1,6 @@
 import subprocess
 import time
+from datetime import datetime, timedelta, timezone
 import importlib
 import json
 import sys, os
@@ -30,6 +31,7 @@ class OuterLayer():
             "Suspicious Port Activity": 0.1,
             "SSH login":                0.3,
             "Possible Phishing":     0.4,
+            "Possibly Bot Army": 0.4,
         }
 
         self.ipBanList = []
@@ -71,6 +73,8 @@ class OuterLayer():
                 self.analyze_ssh_logins()
                 
                 self.analyze_Websocket_Detection()
+
+                self.analyze_BotNet()
 
                 ###### Analyzer Functions ######
                 
@@ -229,6 +233,33 @@ class OuterLayer():
                 self.add_threat(ip, logName, event['geolocation'], event['timestamp'], threatName)
             
             self.database.execute_query(f"UPDATE hybrid_idps.outerLayer SET processed = True WHERE ip_address = '{ip}' AND event_type = '{event_type}'")
+    
+
+    def analyze_BotNet(self):
+            event_type = 'WebSocket Connection'
+            threatName = "Possibly Bot Army"
+            threshold = 2
+
+            results = self.database.execute_query(f"SELECT ip_address, geolocation FROM hybrid_idps.outerLayer WHERE event_type = '{event_type}' AND timestamp >= NOW() - INTERVAL 5 SECOND AND processed = False ORDER BY timestamp DESC")
+
+            result_dict = {}
+
+            for result in results:
+                geolocation = result['geolocation']
+                if geolocation in result_dict:
+                    result_dict[geolocation].append(result)
+                else:
+                    result_dict[geolocation] = [result]
+
+            thresholded_locations = {key: [x['ip_address'] for x in value] for key, value in result_dict.items() if len(value) >= threshold} #The keys of 
+
+            if len(thresholded_locations) > 0:
+                
+                for key in thresholded_locations:
+                    print(thresholded_locations[key][0])
+                    self.add_threat(thresholded_locations[key][0], f"Bots-{datetime.now(timezone.utc)}", key, datetime.now(timezone.utc), threatName)
+                    #self.database.execute_query(f"UPDATE hybrid_idps.outerLayer SET processed = True WHERE ip_address = '{thresholded_locations[key][0]}' AND event_type = '{event_type}'")
+            
 
     # def display_Events_and_calc_threat_level(self):
     #     for ip, deviceData in self.devices.items():
